b8dea1b3605adf09a5c432abcea0dc13143363d3
xiaozhang <3470818183@qq.com>
2024-04-07 14:03:34 +0800

    员工分页查询

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,9 +2,11 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
-public interface                                       EmployeeService {
+public interface EmployeeService {
 
     /**
      * 员工登录
@@ -14,4 +16,6 @@ public interface                                       EmployeeService {
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
     void save(EmployeeDTO employeeDTO);
+
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
